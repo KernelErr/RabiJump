2172bab08416d8a49d518c4dd45685cd3d6cbb1e
ChocoLZS <chocoielzs@gmail.com>
2022-08-23 19:26:51 +0800

    Test web page.

---
+++ server/static/assets/Home.78bc5386.js
@@ -0,0 +1,1 @@
+import{a as e,Q as r}from"./index.ac25c197.js";function a(){return e(r,{children:"this is home"})}export{a as default};
---
+++ server/static/assets/Home.b2b16dde.js
@@ -0,0 +1,1 @@
+import{a as e,Q as r}from"./index.7b5bcad8.js";function a(){return e(r,{children:"this is home"})}export{a as default};
